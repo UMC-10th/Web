347a483f2f7c2c03f5e6af16bfa9d9e6cec59d96
jun <wjo8703@naver.com>
2026-04-04 01:15:49 +0900

    [week3/mission3] useParams를 활용한 영화 상세 페이지 개발

---
+++ jun/Week3/mission3/chapter3-movie/src/types/movie.ts
@@ -0,0 +1,46 @@
+export type Movie={
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+
+export type MovieResponse = {
+page : number;
+results : Movie[];
+total_pages : number;
+total_results : number;
+};
+
+export interface MovieDetail {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  vote_average: number;
+  release_date: string;
+  genres: { id: number; name: string }[];
+  runtime: number;
+}
+
+export interface Cast {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+}
+
+export interface CreditResponse {
+  cast: Cast[];
+}
