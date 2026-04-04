03eed0ff484ee5ab332bcd04a9edbc88d2a0c2e8
lamiro3 <kktthh4076@gmail.com>
2026-04-04 10:47:23 +0900

    [week3/mission] useEffect를 활용한 영화목록 페이지 구현

---
+++ tyeon/Week3/Mission_1/src/components/Card.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import type { Movie } from "../types/movie";
+
+interface Imovie {
+    movie: Movie
+}
+
+export default function Card({movie}: Imovie) {
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+
+    return (
+        <div className="
+            relative 
+            border-transparent 
+            rounded-2xl
+            shadow-lg
+            overflow-hidden
+            cursor-pointer
+            w-40
+            h-60
+            m-4
+            transition-transform duration-500 hover:scale-105
+        "
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        >
+            <img 
+                src={import.meta.env.VITE_TMDB_IMG_BASE_URL+movie.poster_path} 
+                alt={movie.title}
+                className="object-fill h-full"
+            />
+
+            {isHovered && (
+                <div className="
+                    absolute 
+                    inset-0 
+                    bg-gradient-to-t 
+                    from-black/50
+                    transparent
+                    backdrop-blur-md
+                    text-white
+
+                    flex
+                    flex-col
+                    justify-center
+                    items-center
+
+                    p-4
+                ">
+                    <h2 className="
+                        text-lg
+                        font-bold
+                        text-center
+                        leading-snug
+                    ">
+                        {movie.title}
+                    </h2>
+                    <p className="
+                        text-sm
+                        text-gray-300
+                        leading-relaxed
+                        mt-2
+                        line-clamp-5
+                    ">
+                        {movie.overview}
+                    </p>
+                </div>
+            )}
+        </div>
+    )
+}
