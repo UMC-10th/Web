c78c309ce20bb0b11c3ddbea91a5c53d3a473dec
goeun <grace4986@naver.com>
2026-04-04 22:38:35 +0900

    [week3/mission3] useParams를 활용한 영화 상세 페이지 개발

--- week3/src/types/movie.ts
+++ week3/src/types/movie.ts
@@ -15,6 +15,30 @@ export type Movie = {
   vote_count: number;
 };
 
+export interface Cast {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+};
+
+export interface MovieCredits {
+  cast: Cast[];
+};
+
+export interface MovieDetail {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  vote_average: number;
+  release_date: string;
+  tagline: string;
+  runtime: number;
+  genres: { id: number; name: string }[];
+}
+
 export type MovieResponse = {
   page: number;
   results: Movie[];
