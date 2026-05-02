acc97025b3d6116ea3ba485f55b909eeb63b8645
miro-oss <roxkstar@isieun-ui-iMac.local>
2026-05-03 05:45:08 +0900

    [Week5/mission3] Social Login Google 직접 구현해보기

---
+++ Week5/m3/src/App.tsx
@@ -0,0 +1,52 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import type { RouteObject } from 'react-router-dom';
+import './App.css'
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import HomeLayout from './layouts/HomeLayout';
+import SignupPage from './pages/SignupPage';
+import Mypage from './pages/Mypage';
+import { AuthProvider } from './context/AuthContext';
+import ProtextedLayout from './layouts/ProtectedLayout';
+import GoogleLoginRedirectPage from './pages/GoogleLoginRedirectPage';
+
+const publicRoutes:RouteObject[] = [
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {index: true, element: <HomePage />},
+      {path: 'login', element: <LoginPage />},
+      {path: 'signup', element: <SignupPage />},
+      {path: "v1/auth/google/callback", element: <GoogleLoginRedirectPage />},
+    ],
+  }
+]; 
+
+const protextedRoutes:RouteObject[] = [
+  {
+    path:'/',
+    element: <ProtextedLayout />,
+    errorElement: <NotFoundPage />,
+    children:[
+      {
+        path: 'my',
+        element: <Mypage />,
+      }
+    ]
+  }
+]
+
+const router = createBrowserRouter([...publicRoutes, ...protextedRoutes]);
+
+function App() {
+  return ( 
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+}
+
+export default App
